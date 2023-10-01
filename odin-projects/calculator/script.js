ddb137c57fc0bdcb2867faef143b617a5bda33c4
simi2817 <sreenivass@TM4112.local>
2023-10-01 09:23:27 +0100

    version 1 - calculator

---
+++ odin-projects/calculator/script.js
@@ -0,0 +1,162 @@
+let operator = '';
+let firstChoice = '';
+let secondChoice = '';
+let result = 0;
+let operatorON = false;
+
+const assignOperands = (e) => {
+    if(!operatorON) {
+        firstChoice += e.target.id;
+        display('firstNum');
+        return firstChoice;
+    }
+    else {
+        secondChoice += e.target.id;
+        display('secondNum');
+        return secondChoice;
+    }
+}
+
+const assignOperator = (e) => { 
+    return [
+            operator = e.target.id,
+            display('operator'),
+            operatorON = true
+        ];
+}
+
+const operate = () => {
+    const firstOperand = parseFloat(firstChoice);
+    const secondOperand = parseFloat(secondChoice);
+
+    switch(operator) {
+        case '+': 
+            result = (firstOperand + secondOperand).toFixed(2);
+            break;
+        case '-':
+            result = (firstOperand - secondOperand).toFixed(2);
+            break;
+        case '÷':
+            if(secondOperand === 0)
+                alert('Divide by zero error encountered!');
+            else    
+                result = (firstOperand / secondOperand).toFixed(2);
+            break;
+        case 'x':
+            result = (firstOperand * secondOperand).toFixed(2);
+            break;
+    }
+    if(secondOperand === 0 && operator === '÷') {
+        display('clear');
+        clear();
+    }
+    else    display('result')
+    return result;
+}
+
+const display = (value) => {
+    switch(value) {
+        case 'firstNum':
+            displayPanel.innerHTML = firstChoice;
+            break;
+        case 'secondNum':
+            displayPanel.innerHTML = `${firstChoice} ${operator} ${secondChoice}`;
+            break;
+        case 'operator':
+            displayPanel.innerHTML = `${firstChoice} ${operator}`;
+            break;
+        case 'result':
+            displayPanel.innerHTML = `${firstChoice} ${operator} ${secondChoice} = ${result}`;
+            break;
+        case 'clear':
+            displayPanel.innerHTML = '';
+            break;
+    }
+}
+
+const clear = () => {
+    return [
+        firstChoice = '',
+        secondChoice = '',
+        operatorON = false,
+        display('clear')
+    ];
+}
+
+const backSpaceToRemove = () => {
+    if(!secondChoice && !operatorON) {
+            firstChoice = firstChoice.slice(0,-1);
+            display('firstNum');
+            return firstChoice;
+    }
+    else if(operatorON) {
+        if(secondChoice) {
+            secondChoice = secondChoice.slice(0,-1);
+            display('secondNum');
+            return secondChoice;
+        }
+        else {
+            operator = '';
+            display('operator');
+            operatorON = false;
+            return operator;
+        }
+    }
+}
+
+const displayPanel = document.getElementById('displayPanel');
+displayPanel.classList.add('displayPanel');
+
+const one = document.getElementById('1');
+one.addEventListener('click', assignOperands);
+
+const two = document.getElementById('2');
+two.addEventListener('click', assignOperands);
+
+const three = document.getElementById('3');
+three.addEventListener('click', assignOperands);
+
+const four = document.getElementById('4');
+four.addEventListener('click', assignOperands);
+
+const five = document.getElementById('5');
+five.addEventListener('click', assignOperands);
+
+const six = document.getElementById('6');
+six.addEventListener('click', assignOperands);
+
+const seven = document.getElementById('7');
+seven.addEventListener('click', assignOperands);
+
+const eight = document.getElementById('8');
+eight.addEventListener('click', assignOperands);
+
+const nine = document.getElementById('9');
+nine.addEventListener('click', assignOperands);
+
+const zero = document.getElementById('0');
+zero.addEventListener('click', assignOperands);
+
+const add = document.getElementById('+');
+add.addEventListener('click', assignOperator);
+
+const subtract = document.getElementById('-');
+subtract.addEventListener('click', assignOperator);
+
+const division = document.getElementById('÷');
+division.addEventListener('click', assignOperator);
+
+const multiplication = document.getElementById('x');
+multiplication.addEventListener('click', assignOperator);
+
+const calculate = document.getElementById('=');
+calculate.addEventListener('click', operate);
+
+const decimal = document.getElementById('.');
+decimal.addEventListener('click', assignOperands);
+
+const clearDisplay = document.getElementById('clear');
+clearDisplay.addEventListener('click', clear);
+
+const backspace = document.getElementById('backspace');
+backspace.addEventListener('click', backSpaceToRemove);
